209c3caa37d7aada4acfadfb5600a3a07e0c00bb
Javier <javicervel250@gmail.com>
2024-06-22 07:27:38 -0600

    space

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,20 +1,23 @@
 export default function Nav() {
   return (
     <>
-      <header className="fixed   w-full z-10">
-        <nav className="  bg-[#222222] text-white font-semibold flex justify-center text-center items-center  h-[82.18px] p-2">
-          <a href="/#home" className=" ml-10 hover:text-[#C5C5C5]">
+      <header className="fixed    w-full z-10">
+        <nav className=" bg-[#222222] text-white font-semibold flex justify-center text-center items-center  h-[82.18px]">
+          <a href="/#home" className=" md:ml-10 ml-5 hover:text-[#C5C5C5]">
             Home
           </a>
-          <a href="/#proyectos" className="ml-10 hover:text-[#C5C5C5]">
+          <a href="/#proyectos" className="md:ml-10 ml-5 hover:text-[#C5C5C5]">
             Proyectos
           </a>
-          <a href="/#experiencia" className="ml-10 hover:text-[#C5C5C5]">
+          <a
+            href="/#experiencia"
+            className="md:ml-10 ml-5 hover:text-[#C5C5C5]"
+          >
             Experiencia
           </a>
           <a
             href="mailto:javicervel25@gmail.com"
-            className="ml-10 pr-7 hover:text-[#C5C5C5]"
+            className="md:ml-10 ml-5 hover:text-[#C5C5C5]"
           >
             Contacto
           </a>
